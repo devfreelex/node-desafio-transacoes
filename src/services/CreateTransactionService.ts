d22535e54cce7107457cc051eebfd6f651b1cc88
ROdrigo Rocha <rodrigorocha@pop-os.localdomain>
2020-11-25 00:25:55 -0300

    parkless: complete chalenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  type: 'income' | 'outcome';
+  value: number;
+  title: string;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ type, title, value }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid.');
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enough balance.');
+    }
+
+    return this.transactionsRepository.create({
+      type,
+      title,
+      value,
+    });
   }
 }
 
